874c73804edc170af36572e3918448f6e91916d7
NanoSector <rick@nanosector.nl>
2021-05-31 22:03:53 +0200

    Initial commit
    Signed-off-by: NanoSector <rick@nanosector.nl>

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+# Starship
+starship init fish | source
+
+# PATH
+fish_add_path ~/.bin
+
+# Environment variables
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+# Fish configuration
+set fish_greeting
+
+# Aliases
+alias l="exa --long --header --git --grid"
+alias tree="exa -T --long"
+
+# Tool replacements
+alias ls="l"
+alias nano="micro"
+alias cat="bat"
---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+source /usr/share/fzf/shell/key-bindings.fish
+
+function fish_user_key_bindings
+	fzf_key_bindings
+end
